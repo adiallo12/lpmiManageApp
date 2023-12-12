53417f0aa545db45847a8b21fd91744dd0332669
manel <manel_addi@etu.u-bourgogne.fr>
2023-12-12 16:53:04 +0100

    feat add RegistrationScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lpmi/introduction_screen.dart';
+import 'package:lpmi/registrationScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const IntroductionScreen(),
+      home: const RegistrationScreen(),
     );
   }
 }

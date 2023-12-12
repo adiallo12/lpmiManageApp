d9aa15ea40bca7c930c1ef4b78a74a0721303dde
ronan <ronan.lesturgeon@hotmail.com>
2023-12-12 16:14:02 +0100

    feat: rename and add button

---
+++ lib/login_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Image(
+              image: AssetImage("assets/images/logoUB.png"),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            const SizedBox(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 100),
+                  ),
+                  labelText: 'Username',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            const SizedBox(
+              width: 300,
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 100),
+                  ),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Connexion'),
+
+            ),
+      ElevatedButton(
+        onPressed: () {},
+        child: const Text('Sinscire'),
+      )
+          ],
+        ),
+      ),
+    );
+  }
+}

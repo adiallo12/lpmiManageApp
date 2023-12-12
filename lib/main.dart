35f93709d41abeb2d3f016166b9d9d5e9ea72a65
Hector Barre <hector.barre@protonmail.com>
2023-12-12 16:36:10 +0100

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lpmi/connexion_screen.dart';
 import 'package:lpmi/introduction_screen.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const IntroductionScreen(),
+      home: const ConnexionScreen(),
     );
   }
 }

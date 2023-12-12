e15debf154b44ecc181e1ebc00d2f6214d53d730
L-Soyeux <Lilian_Soyeux@etu.u-bourgogne.fr>
2023-12-12 16:08:00 +0100

    Add registration page

--- lib/introduction_screen.dart
+++ lib/introduction_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lpmi/register_screen.dart';
 
 class IntroductionScreen extends StatefulWidget {
   const IntroductionScreen({super.key});
@@ -23,7 +24,12 @@ class _IntroductionScreenState extends State<IntroductionScreen> {
             ),
             const SizedBox(height: 50),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegisterScreen()),
+                );
+              },
               child: const Text("S'inscrire"),
               style: ElevatedButton.styleFrom(
                 padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 12),
@@ -33,14 +39,21 @@ class _IntroductionScreenState extends State<IntroductionScreen> {
               height: 24,
             ),
             ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 12),
-                ),
-                child: const Text("Se connecter"))
+              onPressed: () {
+                
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 12),
+              ),
+              child: const Text("Se connecter"),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+
+
+
---
+++ lib/register_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:lpmi/introduction_screen.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  String _selectedGender = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inscription'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const TextField(
+              decoration: InputDecoration(labelText: 'Nom'),
+            ),
+            const SizedBox(height: 16.0),
+            const TextField(
+              decoration: InputDecoration(labelText: 'Adresse e-mail'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16.0),
+            const TextField(
+              decoration: InputDecoration(labelText: 'Mot de passe'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16.0),
+            const TextField(
+              decoration: InputDecoration(labelText: 'Confirmation Mot de passe'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16.0), 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Radio(
+                  value: 'Homme',
+                  groupValue: _selectedGender,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedGender = value!;
+                    });
+                  },
+                ),
+                const Text('Homme'),
+                const SizedBox(width: 16.0), 
+                Radio(
+                  value: 'Femme',
+                  groupValue: _selectedGender,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedGender = value!;
+                    });
+                  },
+                ),
+                const Text('Femme'),
+              ],
+            ),
+            const SizedBox(height: 16.0), // Espace entre les boutons radios et le bouton d'inscription
+            ElevatedButton(
+              onPressed: () {
+                // Ajoutez ici le code pour traiter l'inscription
+                // Vous pouvez utiliser les données saisies dans les champs
+              },
+              child: const Text('S\'inscrire'),
+            ),
+            const SizedBox(height: 16.0), // Espace entre le bouton d'inscription et le bouton de retour
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const IntroductionScreen()),
+                );
+              },
+              child: const Text('Vous avez déjà un compte ? Connectez-vous'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+

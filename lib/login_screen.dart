1336ce65ab5ed0f907b8d85d41c4611036b50a22
Tristan <tristansabatier1804@gmail.com>
2023-12-12 16:48:36 +0100

    feat:Add Text Button + Redirect

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -12,50 +12,53 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Image.asset("assets/images/logoUB.png", width: 150),
-      const Text(
-        "Se connecter",
-        style: TextStyle(
-          fontSize: 25,
-          decoration: TextDecoration.underline,
-          decorationColor: Colors.orange,
-          color: Colors.orange,
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Image.asset("assets/images/logoUB.png", width: 150),
+        const Text(
+          "Se connecter",
+          style: TextStyle(
+            fontSize: 25,
+            decoration: TextDecoration.underline,
+            decorationColor: Colors.orange,
+            color: Colors.orange,
+          ),
         ),
-      ),
-      const SizedBox(height: 20),
-      const SizedBox(
-        width: 300,
-        child: TextField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), labelText: "Email")),
-      ),
-      const SizedBox(height: 10),
-      const SizedBox(
-        width: 300,
-        child: TextField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), labelText: "Mot de passe")),
-      ),
-      const SizedBox(height: 20),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(foregroundColor: Colors.orange),
-          child: const Text("Se connecter !"),
+        const SizedBox(height: 20),
+        const SizedBox(
+          width: 300,
+          child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Email")),
         ),
+        const SizedBox(height: 10),
+        const SizedBox(
+          width: 300,
+          child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Mot de passe")),
+        ),
+        const SizedBox(height: 20),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(foregroundColor: Colors.orange),
+            child: const Text("Se connecter !"),
+          ),
+        ]),
+        const SizedBox(height: 10),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Pas de compte ? S'inscrire !",
+              style: TextStyle(
+                fontSize: 15,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.orange,
+                color: Colors.orange,
+              )),
+        )
       ]),
-      const SizedBox(height: 10),
-      const Text(
-        "Pas de compte ? S'inscrire !",
-        style: TextStyle(
-          fontSize: 15,
-          decoration: TextDecoration.underline,
-          decorationColor: Colors.orange,
-          color: Colors.orange,
-        ),
-      ),
-    ])));
+    ));
   }
 }

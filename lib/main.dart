4c61dda8af874096f26b11f255743cb073438265
Furax420 <tristan.mayer96@gmail.com>
2023-12-12 16:01:31 +0100

    Formulaire fini

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lpmi/screens/home_screen.dart';
-
-import 'package:lpmi/screens/entreprise_screen.dart';
-import 'package:lpmi/screens/settings_screen.dart';
-import 'package:lpmi/screens/students_screen.dart';
-import 'package:lpmi/screens/offers_screen.dart';
+import 'package:lpmi/signup_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,13 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
-      routes: {
-        '/entreprise': (context) => EntrepriseScreen(),
-        '/students': (context) => StudentsScreen(),
-        '/offers': (context) => OffersScreen(),
-        '/settings': (context) => SettingsScreen(),
-      },
+      home: SignupScreen(),
     );
   }
 }

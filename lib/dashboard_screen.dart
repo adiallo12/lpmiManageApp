f199166961a78a2870e42503320d26a37891bdd5
ronan <ronan.lesturgeon@hotmail.com>
2023-12-23 23:52:30 +0100

    feat: finalization

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'components/side_menu.dart';
-import 'package:fl_chart/fl_chart.dart';
-//import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 //import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -14,73 +13,89 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
+    final DeviceSize = MediaQuery.of(context).size;
+    final mark = DateTime.now();
+    final markedly = mark.day;
+    final markmonth = mark.month;
+    final markyear = mark.year;
     return Scaffold(
-      drawer: SideMenu(
-        onItemsSelected: (int index) {},
-        selectedIndex: 1,
-      ),
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      body: Container(
-        child: Row(
-            children: [
-              Container(
-                width: 125,
-                height: 200,
-                padding: const EdgeInsets.all(5),
-                child: BarChart(
-                  BarChartData(
-                    alignment: BarChartAlignment.spaceAround,
-                    maxY: 20,
-                    barTouchData: BarTouchData(
-                      enabled: false,
-                    ),
-                    titlesData: const FlTitlesData(
-                      show: true,
-
-                    ),),
-                ),
-              ),
-
-              Container(
-                width: 125,
-                height: 200,
-                padding: const EdgeInsets.all(5),
-                child: BarChart(
-                  BarChartData(
-                    alignment: BarChartAlignment.spaceAround,
-                    maxY: 20,
-                    barTouchData: BarTouchData(
-                      enabled: false,
-                    ),
-                    titlesData: const FlTitlesData(
-                      show: true,
-                    ),
-                  ),
-                ),),
-              Container(
-
-                width: 125,
-                height: 200,
-                padding: const EdgeInsets.all(5),
-                child: BarChart(
-                  BarChartData(
-                    alignment: BarChartAlignment.spaceAround,
-                    maxY: 20,
-                    barTouchData: BarTouchData(
-                      enabled: false,
-                    ),
-                    titlesData: const  FlTitlesData(
-                      show: true,
-
-                    ),
-                  ),
-                ),
-              ),
-            ]
+        drawer: SideMenu(
+          onItemsSelected: (int index) {},
+          selectedIndex: 1,
         ),
-      ),
-    );
+        appBar:  AppBar(
+          title: const Text('Dashboard',textAlign: TextAlign.center),
+        ),
+        body: Container(
+          height: DeviceSize.height / 2,
+          width: DeviceSize.width,
+          child: Column(
+            children: [
+              Text('Nous sommes le $markedly/$markmonth/$markyear.',textAlign: TextAlign.left,),
+              Row(children: <Widget>[
+                Expanded(
+                  flex: 3,
+                  child: SfCartesianChart(
+                    primaryXAxis: const CategoryAxis(),
+                    legend: const Legend(isVisible: false),
+                    title:const  ChartTitle(text: "Nombres D'etudiants"),
+                    series: <CartesianSeries>[
+                      ColumnSeries<ChartData, String>(
+                          dataSource: <ChartData>[
+                            ChartData('Data', 120),
+                          ],
+                          xValueMapper: (ChartData data, _) => data.x,
+                          yValueMapper: (ChartData data, _) => data.y,
+                          dataLabelSettings:
+                          const DataLabelSettings(isVisible: false))
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: SfCartesianChart(
+                    primaryXAxis: const CategoryAxis(),
+                    legend: const Legend(isVisible: false),
+                    title: const ChartTitle(text: "Nombres D'Offres"),
+                    series: <CartesianSeries>[
+                      ColumnSeries<ChartData, String>(
+                          dataSource: <ChartData>[
+                            ChartData('Data', 75),
+                          ],
+                          xValueMapper: (ChartData data, _) => data.x,
+                          yValueMapper: (ChartData data, _) => data.y,
+                          dataLabelSettings:
+                          const DataLabelSettings(isVisible: false))
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: SfCartesianChart(
+                    primaryXAxis: const CategoryAxis(),
+                    legend: const Legend(isVisible: false),
+                    title: const ChartTitle(text: "Nombres D'Entreprises"),
+                    series: <CartesianSeries>[
+                      ColumnSeries<ChartData, String>(
+                          dataSource: <ChartData>[
+                            ChartData('Data', 20),
+                          ],
+                          xValueMapper: (ChartData data, _) => data.x,
+                          yValueMapper: (ChartData data, _) => data.y,
+                          dataLabelSettings:
+                          const DataLabelSettings(isVisible: false))
+                    ],
+                  ),
+                ),
+              ]),
+            ],
+          ),
+        ));
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final String x;
+  final double y;
 }
